2952e4a7e8f70d27ef6175e7631ae327e0a850dd
Fraser <tomatoesneverlie@gmail.com>
2023-06-02 18:49:03 -0400

    write binding for `Prism.highlight`

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,45 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use quick_js::Context;
+
+const PRISM_JS: &str = include_str!("../prism/prism.js");
+
+/// initialize prism.js
+pub fn init() -> Context {
+    let context = Context::new().unwrap();
+    context.eval(PRISM_JS).unwrap();
+    // context.eval("function highlight(text, grammar, language) { return Prism.highlight(text, grammer, language); }").unwrap();
+    context
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+/// Accepts a string of text as input, and returns a string with the HTML produced.
+/// text: the code to be highlighted
+/// grammar: the name of the prism.js language definition in the context
+/// language: the name of the language definition passed to grammar
+///
+/// Example:
+/// ```rust
+/// use prism_js::{init, highlight_internal};
+///
+/// let mut context = init();
+///
+/// let text = "var foo = true;";
+/// let grammar = "Prism.languages.javascript";
+/// let language = "javascript";
+///
+/// let html = highlight_internal(&mut context, text, grammar, language);
+/// assert!(html.is_some());
+/// assert!(html.unwrap() == r#"<span class="token keyword">var</span> foo <span class="token operator">=</span> <span class="token boolean">true</span><span class="token punctuation">;</span>"#);
+/// ```
+pub fn highlight_internal(
+    context: &mut Context,
+    text: &str,
+    grammar: &str,
+    language: &str,
+) -> Option<String> {
+    // context.call_function doesn't work here since the actual value for grammer is too large
+    context.set_global("text", text).ok()?;
+    context.set_global("language", language).ok()?;
+    context
+        .eval(&format!("Prism.highlight(text, {}, language)", grammar))
+        .ok()
+        .and_then(|v| v.as_str().map(|s| s.to_string()))
 }
